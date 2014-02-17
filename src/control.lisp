68173a4532f1fb2b21fc70a162d7ca867c525016
HerculesShek <hercules.shek@gmail.com>
2014-02-17 16:20:17 +0800

    add let test

---
+++ src/control.lisp
@@ -0,0 +1,69 @@
+(defun read-integer (str)
+  (let ((accum 0))
+	(dotimes (pos (length str))
+	  (let ((i (digit-char-p (char str pos))))
+		(if i
+			(setf accum (+ (* accum 10) i))
+			(return-from read-integer nil))))
+	accum))
+
+(defun our-member (obj lst)
+  (if (atom lst)
+	  nil
+	  (if (eql (car lst) obj)
+		  lst
+		  (our-member obj (cdr lst)))))
+
+(defun our-member-cond (obj lst)
+  (cond ((atom lst) lst)
+		((eql obj (car lst)) lst)
+		(t (our-member-cond obj (cdr lst)))))
+
+(defun month-length (mon)
+  (case mon
+	((jan mar may jul aug oct dec) 31)
+	((apr jun sept nov) 30)
+	(feb (if (leap-year (progn (format t "Please enter the year:~%") (read))) 29 28))
+	(otherwise "unknown month")))
+
+(defun leap-year (y)
+  (if (numberp y)
+	  (if (typep (/ y 400) 'integer)
+		  t
+		  (if (typep (/ y 100) 'integer)
+			  nil
+			  (if (typep (/ y 4) 'integer)
+				  t)))))
+
+(defun what-is-it (x)
+  (format t "~&~S is ~A.~%"
+		  x (typecase x
+			  (float "a float")
+			  (null "a symbol, boolean false, or the empty list")
+			  (list "a list")
+			  (t (format nil "a(n) ~(~A~)" (type-of x))))))
+
+
+(defun let-test ()
+  (let ((x 'a))
+	(do ((x 1 (+ x 1))
+		 (y x x))
+		((> x 5))
+	  (format t "(~A ~A) " x y))
+	x))
+
+
+(defun let-test-1 () 
+  (do ((m 1 (+ m 1))
+	   (n m m)) ;; ERROR! 没有m的定义！
+	  ((> m 5))
+	(format t "(~A ~A) " m n)))
+
+
+(defun let-test-2 ()
+  (do ((x 1 (incf x))
+	   (y x x))
+	  ((> x 5))
+	(format t "(~A ~A) " x y))
+  x)
+
