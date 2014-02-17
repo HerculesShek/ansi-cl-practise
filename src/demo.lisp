d1cb5b9023e634484ef77a89fcb41908b08462d7
HerculesShek <hercules.shek@gmail.com>
2014-02-17 09:33:20 +0800

    add block test

--- src/demo.lisp
+++ src/demo.lisp
@@ -3,5 +3,13 @@
 	  1
 	  (+ (fabo (- n 1)) (fabo (- n 2))))) 
 
+(defun labels-test ()
+  (labels ((f2 (c d) (+ (f1 100 c) d))
+		   (f1 (a b) (+ a b)))
+	(+ (f1 2 3) (f2 3 4))))
+
 (defun main()
+  (labels-test))
   
+(defun re-test()
+  (return re-test 'a))
