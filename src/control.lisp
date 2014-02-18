0277043b6cb0afd8d9033c77c0f07707d5b15295
HerculesShek <hercules.shek@gmail.com>
2014-02-18 16:20:21 +0800

    add date arithmetic

--- src/control.lisp
+++ src/control.lisp
@@ -23,17 +23,13 @@
   (case mon
 	((jan mar may jul aug oct dec) 31)
 	((apr jun sept nov) 30)
-	(feb (if (leap-year (progn (format t "Please enter the year:~%") (read))) 29 28))
+	(feb (if (leap? (progn (format t "Please enter the year:~%") (read))) 29 28))
 	(otherwise "unknown month")))
 
-(defun leap-year (y)
-  (if (numberp y)
-	  (if (typep (/ y 400) 'integer)
-		  t
-		  (if (typep (/ y 100) 'integer)
-			  nil
-			  (if (typep (/ y 4) 'integer)
-				  t)))))
+(defun leap? (y)
+  (and (zerop (mod y 4))
+	   (or (zerop (mod y 400))
+		   (not (zerop (mod y 100))))))
 
 (defun what-is-it (x)
   (format t "~&~S is ~A.~%"
@@ -86,3 +82,87 @@
 			(format t "~A ~A" x y))
 		'(hip flip slip)
 		'(hop flop slop)))
+
+
+(defun super ()
+  (catch 'abort
+	(sub)
+	(format t "We'll never see this.")))
+
+(defun sub ()
+  (throw 'abort 99))
+
+
+(defun factorial (x)
+  (cond ((or (not (typep x 'integer)) (minusp x))
+		 (error "~S is not a valid argument to FACTORIAL." x))
+		((zerop x) 1)
+		(t (* x (factorial (- x 1))))))
+
+
+(defun unwind-protect-test ()
+	(let ((var 10))
+	  (setf var (catch 'cat
+				  (upt-sub var)
+				  12))
+	  var))
+
+(defun upt-sub (var)
+  (unwind-protect
+	   (throw 'cat 13)
+	(incf var)
+	(format t "The value of var is ~A" var)))
+  
+  
+;; Date Arithmetic 
+(defconstant month #(0 31 59 90 120 151 181 212 243 273 304 334 365))
+(defconstant yzero 2000)
+
+(defun day->num (d m y)
+  (+ (- d 1) (month-num m y) (year-num y)))
+
+(defun month-num (m y)
+  (+ (svref month (- m 1))
+	 (if (and (> m 2) (leap? y)) 1 0)))
+
+(defun year-num (y)
+  (let ((d 0))
+	(if (>= y yzero)
+		(dotimes (i (- y yzero) d)
+		  (incf d (year-days (+ yzero i))))
+		(dotimes (i (- yzero y) (- d))
+		  (incf d (year-days (+ y i)))))))
+
+(defun year-days (y)
+  (if (leap? y) 366 365))
+
+
+(defun num->date (n)
+  (multiple-value-bind (y left) (num-year n)
+	(multiple-value-bind (d m) (num-month left y)
+	  (values d m y))))
+
+(defun num-year (n)
+  (if (> n 0)
+	  (do* ((y yzero (+ y 1))
+			(prev 0 d)
+			(d (year-days y) (+ d (year-days y))))
+		   ((> d n) (values y (- n prev))))
+	  (do* ((y (- yzero 1) (- y 1))
+			(d (- (year-days y)) (- d (year-days y))))
+		   ((<= d n) (values y (- n d))))))
+
+(defun num-month (n y)
+  (if (leap? y)
+	  (cond ((= n 59) (values 2 29))
+			((> n 59) (nmon (- n 1)))
+			(t (nmon n)))
+	  (nmon n)))
+
+(defun nmon (n)
+  (let ((m (position n month :test #'<)))
+	(values m (+ 1 (- n (svref month (- m 1)))))))
+
+
+(defun date+ (d m y n)
+  (num->date (+ n (day->num d m y))))
