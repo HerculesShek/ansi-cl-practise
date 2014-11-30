963314657b2f9ec66c8c652c37326afcc68fbe1e
HerculesShek <hercules.shek@gmail.com>
2014-11-30 18:23:37 +0800

    add binary search tree test

--- src/specialized-data-structures.lisp
+++ src/specialized-data-structures.lisp
@@ -4,30 +4,30 @@
 (defun bin-search (obj vec)
   (let ((len (length vec)))
     (and (not (zerop len))
-	 (finder obj vec 0 (- len 1)))))
+         (finder obj vec 0 (- len 1)))))
 
 ;; 闭区间 start and end are indexes
 (defun finder (obj vec start end) 
   (let ((range (- end start)))
     (if (zerop range) ; 区间内只有一个元素 base case
-	(if (eql obj (svref vec start)) 
-	    obj
-	    nil)
-	(let ((mid (+ start (round (/ range 2)))))
-	  (let ((obj2 (svref vec mid)))
-	    (if (> obj obj2)  ; 其实这里用cond比较好
-		(finder obj vec (+ mid 1) end)
-		(if (< obj obj2)
-		    (finder obj vec start (- mid 1))
-		    obj)))))))
+        (if (eql obj (svref vec start)) 
+            obj
+            nil)
+        (let ((mid (+ start (round (/ range 2)))))
+          (let ((obj2 (svref vec mid)))
+            (if (> obj obj2)  ; 其实这里用cond比较好
+                (finder obj vec (+ mid 1) end)
+                (if (< obj obj2)
+                    (finder obj vec start (- mid 1))
+                    obj)))))))
 
 ;; 针对向量的回文判断
 (defun mirror? (s)
   (do ((forward 0 (+ forward 1))
        (back (- (length s) 1) (- back 1)))
       ((or (>= forward back)
-	   (not (eql (elt s forward)
-		     (elt s back))))
+           (not (eql (elt s forward)
+                     (elt s back))))
        (>= forward back))))
 
 ;; 将字符串分段，取出第二部分
@@ -41,12 +41,12 @@
 (defun tokens (str test start)
   (let ((p1 (position-if test str :start start)))
     (if p1
-	(let ((p2 (position-if #'(lambda (c)
-				   (not (funcall test c)))
-			       str :start p1)))
-	  (cons (subseq str p1 p2)
-		(if p2
-		    (tokens str test p2)))))))
+        (let ((p2 (position-if #'(lambda (c)
+                                   (not (funcall test c)))
+                               str :start p1)))
+          (cons (subseq str p1 p2)
+                (if p2
+                    (tokens str test p2)))))))
 ;; 判断字符是不是可以显示的字符，不包含空格
 (defun constituent (c)
   (and (graphic-char-p c)
@@ -56,8 +56,8 @@
 (defun parse-date (date-str)
   (let ((toks (tokens date-str #'constituent 0)))
     (list (parse-integer (first toks))
-	  (parse-month (second toks))
-	  (my-parse-integer (third toks)))))
+          (parse-month (second toks))
+          (my-parse-integer (third toks)))))
 ;; 定义月份的常量向量
 (defconstant month-names
   #("jan" "feb" "mar" "apr" "may" "jun"
@@ -66,52 +66,54 @@
 (defun parse-month (str) 
   (let ((p (position str month-names :test #'string-equal)))
     (if p
-	(incf p)
-	nil)))
+        (incf p)
+        nil)))
 ;; 自定义 parse-integer  (my-parse-integer "123") => 123
 (defun my-parse-integer (str)
   (and (every #'digit-char-p str)
        (let ((res 0))
-	 (dotimes (index (length str))
-	   (setf res (+ (* 10 res)
-			(digit-char-p (char str index)))))
-	 res)))
+         (dotimes (index (length str) res)
+           (setf res (+ (* 10 res)
+                        (digit-char-p (char str index))))))))
 
 ;; TODO 功能更加强大的整数转化 加入关键字参数，加入进制
 ;; 加入科学计数表示法，加入+-号
 
 
-;; BST
+;;; BST Binary Search Tree
 ;; node 
 (defstruct (node (:print-function
-		  (lambda (node s d) ; 定义结构时指定打印函数的时候，这个函数必须是接受三个参数
-		    (format s "#<~A>" (node-elt node)))))
+                  (lambda (node s d) ; 打印函数必须接受三个参数
+                    (format s "#<~A>" (node-elt node)))))
   elt
   (l nil)
   (r nil))
 ;; insert 非平衡的
+;; obj---要查入到树中的元素的值 
+;; bst---二叉搜索树
+;; <---表示的是比较函数 不是小于运算符
 (defun bst-insert (obj bst <)
   (if (null bst)
       (make-node :elt obj)
       (let ((elt (node-elt bst)))
-	(if (eql elt obj) ;; 已经存在此元素则不再插入
-	    bst
-	    (if (funcall < obj elt)
-		(make-node :elt elt
-			   :l (bst-insert obj (node-l bst) <)
-			   :r (node-r bst))
-		(make-node :elt elt
-			   :l (node-l bst)
-			   :r (bst-insert obj (node-r bst) <)))))))
+        (if (eql elt obj) ;; 已经存在此元素则不再插入
+            bst
+            (if (funcall < obj elt)
+                (make-node :elt elt
+                           :l (bst-insert obj (node-l bst) <)
+                           :r (node-r bst))
+                (make-node :elt elt
+                           :l (node-l bst)
+                           :r (bst-insert obj (node-r bst) <)))))))
 ;; search
 (defun bst-find (obj bst <)
   (if bst
       (let ((elt (node-elt bst)))
-	(if (eql obj elt)
-	    bst
-	    (if (funcall < obj elt)
-		(bst-find obj (node-l bst) <)
-		(bst-find obj (node-r bst) <))))))
+        (if (eql obj elt)
+            bst
+            (if (funcall < obj elt)
+                (bst-find obj (node-l bst) <)
+                (bst-find obj (node-r bst) <))))))
 ;; min
 (defun bst-min (bst)
   (and bst
@@ -125,40 +127,39 @@
   (if (null (node-l bst))
       (node-r bst)
       (make-node :elt (node-elt bst)
-		 :l (bst-remove-min (node-l bst))
-		 :r (node-r bst))))
+                 :l (bst-remove-min (node-l bst))
+                 :r (node-r bst))))
 ;; remove max 
 (defun bst-remove-max (bst)
   (if (null (node-r bst))
       (node-l bst)
       (make-node :elt (node-elt bst)
-		 :l (node-l bst)
-		 :r (bst-remove-max (node-r bst)))))
-
+                 :l (node-l bst)
+                 :r (bst-remove-max (node-r bst)))))
+;; remove obj from bst
 (defun bst-remove (obj bst <)
   (if bst
       (let ((elt (node-elt bst)))
-	(if (eql obj elt)
-	    (percolate bst)
-	    (if (funcall < obj elt)
-		(make-node :elt elt
-			   :l (bst-remove obj (node-l bst) <)
-			   :r (node-r bst))
-		(make-node :elt elt
-			   :l (node-l bst)
-			   :r (bst-remove obj (node-r bst) <)))))))
+        (if (eql obj elt)
+            (percolate bst)
+            (if (funcall < obj elt)
+                (make-node :elt elt
+                           :l (bst-remove obj (node-l bst) <)
+                           :r (node-r bst))
+                (make-node :elt elt
+                           :l (node-l bst)
+                           :r (bst-remove obj (node-r bst) <)))))))
 (defun percolate (bst)
   (let ((l (node-l bst)) (r (node-r bst)))
     (cond ((null l) r)
-	  ((null r) l)
-	  (t (if (zerop (random 2)) ;; 在这里随机的选择前驱或者是后继来代替
-		 (make-node :elt (node-elt (bst-max l))
-			    :l (bst-remove-max l)
-			    :r r)
-		 (make-node :elt (node-elt (bst-max r))
-			    :l l
-			    :r (bst-remove-min r)))))))
-
+          ((null r) l)
+          (t (if (zerop (random 2)) ;; 这里随机选择前驱或者是后继
+                 (make-node :elt (node-elt (bst-max l))
+                            :l (bst-remove-max l)
+                            :r r)
+                 (make-node :elt (node-elt (bst-max r))
+                            :l l
+                            :r (bst-remove-min r)))))))
 ;; print 
 (defun print-bst (bst)
   (when bst
@@ -172,6 +173,28 @@
     (funcall fn (node-elt bst))
     (bst-traverse fn (node-r bst))))
 
+;; BST的测试函数
+(defun bst-test()
+  (let ((nums))
+    (progn
+      (format t "insert 5 8 4 2 1 9 6 7 3 ...~%")
+      (dolist (x '(5 8 4 2 1 9 6 7 3)) ; insert nums 
+        (setf nums (bst-insert x nums #'<)))
+      (format t "if 12 exists? ~A" 
+              (if (bst-find 12 nums #'<) "yes" "no"))
+      (format t "~%if 9 exists? ~A" 
+              (if (bst-find 9 nums #'<) "yes" "no"))
+      (format t "~%min:~t~A" (bst-min nums))
+      (format t "~%max:~t~A" (bst-max nums))
+      (format t "~%before remove:~%print:~%")
+      (print-bst nums)
+      (format t "bst-traverse:")
+      (bst-traverse #'(lambda (x) (format t "~A " x)) nums)
+      (setf nums (bst-remove 2 nums #'<))
+      (format t "~%after remove 2:~%print:~%")
+      (print-bst nums)
+      (format t "bst-traverse:~%~T")
+      (bst-traverse #'princ nums))))
 
 ;; Exercises
 ;; ex1
@@ -183,11 +206,11 @@
 (defun quarter-turn (arr)
   (and arr
        (let ((dim (array-dimensions arr)))
-	 (let ((d (car dim)) (new-arr (make-array dim)))
-	   (do ((i 0 (incf i))) ((= i d))
-	     (do ((j 0 (incf j))) ((= j d))
-	       (setf (aref new-arr j (- d 1 i)) (aref arr i j))))
-	   new-arr))))
+         (let ((d (car dim)) (new-arr (make-array dim)))
+           (do ((i 0 (incf i))) ((= i d))
+             (do ((j 0 (incf j))) ((= j d))
+               (setf (aref new-arr j (- d 1 i)) (aref arr i j))))
+           new-arr))))
 
 ;; ex2 
 ;; a
@@ -212,9 +235,9 @@
 (defun copy-tree-ex3 (tree)
   (and tree
        (make-my-node :elt (my-node-elt tree)
-		     :left (my-node-left tree)
-		     :middle (my-node-middle tree)
-		     :right (my-node-right tree))))
+                     :left (my-node-left tree)
+                     :middle (my-node-middle tree)
+                     :right (my-node-right tree))))
 (defun value-test (obj tree)
   (if tree
       (or
@@ -227,27 +250,27 @@
 (defun bst-ordered-list (bst)
   (if bst
       (append (bst-ordered-list (node-r bst))
-	      (list (node-elt bst))
-	      (bst-ordered-list (node-l bst)))))
+              (list (node-elt bst))
+              (bst-ordered-list (node-l bst)))))
 ;; labels and ex4-v2 ex4-v3
 ;; labels
 (defun recursive-times (k n)
   (labels ((temp (n) 
-	     (if (zerop n) 0 (+ k (temp (1- n))))))
+             (if (zerop n) 0 (+ k (temp (1- n))))))
     (temp n)))
 ;; ex4-v2
 (defun bst->list (bst0)
   (labels ((rec (bst1 acc)
              (if bst1
                  (rec (node-r bst1) (cons (node-elt bst1) (rec (node-l bst1) acc)))
-		 acc)))
+                 acc)))
     (rec bst0 nil)))
 ;; ex4-v3
 (defun bst->lst (bst0)
   (labels ((rec (bst1 acc)
              (if bst1
                  (rec (node-l bst1) (append (rec (node-r bst1) acc) (list (node-elt bst1))))
-		 acc)))
+                 acc)))
     (rec bst0 nil)))
 
 
@@ -257,17 +280,17 @@
 (defun assoc->hash (a)
   (if a
       (let ((h (make-hash-table)))
-	(dolist (e a)
-	  (setf (gethash (car e) h) (cdr e)))
-	h)))
+        (dolist (e a)
+          (setf (gethash (car e) h) (cdr e)))
+        h)))
 ;; b-v1
 (defun hash->assoc (h)
   (if h
       (let ((lst nil))
-	(maphash #'(lambda (k v)
-		     (setf lst (cons (cons k v) lst)))
-		 h)
-	lst)))
+        (maphash #'(lambda (k v)
+                     (setf lst (cons (cons k v) lst)))
+                 h)
+        lst)))
 ;; b-v2
 (defun hash->lst (ht)
   (let ((acc nil))
