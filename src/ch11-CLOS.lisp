7991f15e9ab49f9e0762cf1db3a88f2ed5421939
HerculesShek <hercules.shek@gmail.com>
2015-02-19 22:15:46 +0800

    CLOS task

--- src/ch11-CLOS.lisp
+++ src/ch11-CLOS.lisp
@@ -28,8 +28,14 @@
   (height width))
 
 (defclass ccircle ()
-  ((radius :accessor circle-radius)
-   (center :accessor circle-center)))
+  ((radius :reader r-radius
+           :writer w-radius
+           :accessor a-radius
+           :initarg :radius
+           :initform 1.0)
+   (center :accessor c-center
+           :initarg :center
+           :initform (cons 0.0 0.0))))
 
 (defmethod carea ((x crectangle))
   (* (slot-value x 'height) (slot-value x 'width)))
@@ -57,5 +63,61 @@
   (let ((c (make-instance 'colored-circle)))
     (slot-value c 'color))) ; error !
 
+;; shared slot 
+(defclass tabloid ()
+  ((top-story :accessor tabloid-story
+              :allocation :class)))
+;; shred slot test 
+(defun shared-slot-test ()
+  (let ((daily-blab (make-instance 'tabloid))
+        (unsolicited-mail (make-instance 'tabloid)))
+    (setf (tabloid-story daily-blab) 'adultery-of-senator)
+    (tabloid-story unsolicited-mail)))
+
+(defclass graphic ()
+  ((color :accessor graphic-color :initarg :color)
+   (visible :accessor graphic-visible :initarg :visible
+            :initform t)))
+(defclass screen-circle (ccircle graphic) 
+  ((color :initform 'purple)))
 
 
+;;; precedence
+(defclass sculpture () (height width depth)) ; 雕塑
+
+(defclass statue (sculpture) (subject)) ; 雕像
+
+(defclass metalwork () (metal-type)) ; 金属制品
+
+(defclass casting (metalwork) ()) ; 铸件
+
+(defclass cast-statue (statue casting) ()) ; 青铜雕像
+
+(defmethod value ((x sculpture)) 1)
+(defmethod value ((x casting)) 4)
+
+
+;;; Generic Fuctions 
+(defmethod combine (x y)
+  (list x y))
+(defclass stuff () ((name :accessor name :initarg :name)))
+(defclass ice-cream (stuff) ())
+(defclass topping (stuff) ())
+
+;; 现在下面是替 combine 定义的第二个方法：
+(defmethod combine ((ic ice-cream) (top topping))
+  (format nil "~A ice-cream with ~A topping."
+          (name ic)
+          (name top)))
+(defmethod combine ((ic ice-cream) x)
+  (format nil "~A ice-cream with ~A."
+          (name ic)
+          x))
+(defmethod combine ((x number) (y number)) 
+(+ x y)) 
+(defmethod combine ((x (eql 'powder)) (y (eql 'spark)))
+  'boom)
+
+;;; Generic Fuctions Test
+(defmethod combine ((a cast-statue) b)
+  (format nil "~A and ~A" (name a) b))
