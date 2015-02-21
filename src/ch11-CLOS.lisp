8c84a1b73f491e53a40e1fcd92baa6e799e1722e
HerculesShek <hercules.shek@gmail.com>
2015-02-21 18:04:35 +0800

    finish review of chapter 11

--- src/ch11-CLOS.lisp
+++ src/ch11-CLOS.lisp
@@ -96,6 +96,17 @@
 (defmethod value ((x sculpture)) 1)
 (defmethod value ((x casting)) 4)
 
+;;; precedence test
+(defmethod add ((a integer) (b integer))
+  (+ a b))
+(defmethod add ((a (eql 3)) b)
+  (+ a b 10))
+(defmethod add ((a integer) (b symbol))
+  42)
+(defun pre-test () ; 17 7 error! The value A is not of the expected type NUMBER.
+  (format t "~A ~A ~A" (add 3 4) (add 4 3) (add 3 'a)))
+;; 直观上看 (add 3 'a)应该调用第三个add方法，但是结果是调用的第二个！因为'a同时属于symbol和t类型
+;; 的情况下，有个eql满足的优先级会高很多！
 
 ;;; Generic Fuctions 
 (defmethod combine (x y)
@@ -204,8 +215,84 @@
 (defmethod area ((x circle))
   (* pi (expt (circle-radius x) 2)))
 
-;; ex2 ch11-CLOS-ex2.lisp
+;; ex2 go to ch11-CLOS-ex2.lisp
 
 ;; ex3 
+;;                h
+;;               / \ 
+;;           e  f   g 
+;;            \  \ /
+;;             \  |
+;;           c   d
+;;            \ /
+;;             a
+;; order of specification (descending): a, c, d, e, f, g, h
+;;                h
+;;               / \ 
+;;           e  f   g 
+;;            \  \ /
+;;             \  |
+;;               d   c
+;;                \ /
+;;                 b
+;; order of specification (descending): b, d, e, f, g, h, c
 
+;; ex4 Suppose that you already have the following  functions; 
+;; `precedence`: takes an object and returns its precedence list, a list of 
+;;   classes ordered from most specific to least specific.  
+;; `methods`:  takes a generic function and returns a list of all its methods. 
+;; `specializations`:  takes a method and returns a list of the specializations 
+;;   of the parameters. Each element of the returned list will be either a 
+;;   class, or a list of the form (eql x), or t (indicating that the 
+;;   parameter is unspecialized). 
+;; Using these functions (and not compute-applicable-methods or find-method),  
+;; define a function most-spec-app-meth that takes a generic function and a 
+;; list of the arguments with which it has been called, and returns the most 
+;; specific applicable method, if any. 
+(defun score-it (meth classlist args)
+  (let ((specs (specialization meth))
+        (score 0))
+    (if (/= (length specs) (length classlist))
+        nil
+        (do ((i 0 (1+ i))
+             (spec (car specs) (car spes))
+             (classes (car classlist) (car clist))
+             (spes (cdr specs) (cdr spes))
+             (clist (cdr classlist) (cdr clist)))
+            ((not spec) score)
+          (if (and (consp spec) (eql (car spec) 'eql))
+              (decf score most-positive-fixnum)
+              (let ((p (position spec classes)))
+                (if p
+                    (incf score (* 10 (1+ p)))
+                    (return-from score-it nil))))))))
 
+(defun most-spec-app-meth (gf args)
+  (let* ((classlist (mapcar #'precedence args)) ; 实参的所有的类别的优先级
+         (all-meths (method gf))
+         (curr-score (score-it (car all-meths) classlist args))
+         (curr-meth (if curr-score (car all-meths) nil)))
+    (dolist (meth (cdr all-meths))
+      (let ((score (score-it meth classlist args)))
+        (if (and score (< score curr-score))
+            (setf curr-score score curr-meth meth))))
+    curr-meth))
+;; TODO 自己写三个函数 precedence method specialization
+   
+;; ex5
+(defparameter *area-counter* 0)
+(defmethod area :before (obj)
+  (declare (ignore obj))
+  (incf *area-counter*))
+
+;; ex7 If a function contains several classes as arguments. For example, 
+;; the following method cannot be defined by the message passing model.
+(defclass stuff () ((name :accessor name :initarg :name)))
+(defclass ice-cream (stuff) ())
+(defclass topping (stuff) ())
+
+;; 现在下面是替 combine 定义的第二个方法：
+(defmethod combine ((ic ice-cream) (top topping))
+  (format nil "~A ice-cream with ~A topping."
+          (name ic)
+          (name top)))
