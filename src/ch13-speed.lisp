e5424b929030c026fd75c5ee7207f9b6678b9c14
HerculesShek <hercules.shek@gmail.com>
2015-02-24 18:20:55 +0800

    rhyming dictionary

--- src/ch13-speed.lisp
+++ src/ch13-speed.lisp
@@ -29,3 +29,75 @@
     (len lst 0)))
 
 
+;; inline function
+(declaim (inline single?))
+
+(defun single? (lst)
+  (and (consp lst) (null (cdr lst))))
+
+(defun foo (x)
+  (single? (bar x)))
+(defun foo (x)
+  (let ((lst (bar x)))
+    (and (consp lst) (null (cdr lst)))))
+(defun bar (x)
+  (list (+ 1 x) (+ 2 x)))
+
+(declaim (type fixnum *count*))
+(declaim (fixnum *count*))
+
+(defun poly (a b x)
+  (declare (fixnum a b x))
+  (+ (* a (expt x 2)) (* b x)))
+
+(defun poly (a b x)
+  (declare (fixnum a b x))
+  (the fixnum (+ (the fixnum (* a (the fixnum (expt x 2))))
+                 (the fixnum (* b x)))))
+
+
+(setf x (vector 1.234d0 2.345d0 3.456d0)
+      y (make-array 3 :element-type 'double-float)
+      (aref y 0) 1.234d0
+      (aref y 1) 2.345d0
+      (aref y 2) 3.456d0)
+
+
+(declare (type (vector fixnum 20) v))
+
+(setf a (make-array '(1000 1000)
+                    :element-type 'single-float
+                    :initial-element 1.0s0))
+
+(defun sum-elts (a)
+  (declare (type (simple-array single-float (1000 1000)) a))
+  (let ((sum 0.0s0))
+    (declare (type single-float sum))
+    (dotimes (r 1000)
+      (dotimes (c 1000)
+        (incf sum (aref a r c))))
+    sum))
+
+;;;  Generating a rhyming dictionary. 
+(defconstant dict (make-array 25000 :fill-pointer 0))
+
+(defun read-words (from)
+  (setf (fill-pointer dict) 0)
+  (with-open-file (in from :direction :input)
+    (do ((w (read-line in nil :eof)
+            (read-line in nil :eof)))
+        ((eql w :eof))
+      (vector-push w dict))))
+
+(defun xform (fn seq) (map-into seq fn seq))
+
+(defun write-words (to)
+  (with-open-file (out to :direction :output
+                          :if-exists :supersede)
+    (map nil #'(lambda (x)
+                 (fresh-line out)
+                 (princ x out))
+             (xform #'nreverse
+                    (sort (xform #'nreverse dict)
+                          #'string<)))))
+
