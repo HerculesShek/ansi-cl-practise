a2a5119607b445b28b1e68a1af1f74f518c13575
HerculesShek <hercules.shek@gmail.com>
2015-03-03 13:02:46 +0800

    read macro task

--- src/ch14-advanced-topics.lisp
+++ src/ch14-advanced-topics.lisp
@@ -1,6 +1,5 @@
 ;;;; this file contains demos and exercise of chapter 14
 
-
 (defun cdr-circular? (lst &optional pres)
   (if (null lst)
       nil
@@ -17,8 +16,8 @@
                              (zerop (mod x ,n))))))
 
 
-;; copy binary files 
-(defun copy-file (from to)
+;; copy files, not only binary  
+(defun my-copy-file (from to)
   (with-open-file (in from :direction :input
                       :element-type 'unsigned-byte)
     (with-open-file (out to :direction :output
@@ -29,7 +28,28 @@
         (declare (fixnum i))
         (write-byte i out)))))
 
-
+;; quote read macro
 (set-macro-character #\'
                      #'(lambda (stream char)
                          (list (quote quote) (read stream t nil t))))
+
+
+(set-dispatch-macro-character #\# #\?
+                              #'(lambda (stream char1 char2)
+                                  (list 'quote
+                                        (let ((lst nil))
+                                          (dotimes (i (+ (read stream t nil t) 1))
+                                            (push i lst))
+                                          (nreverse lst)))))
+
+
+(set-macro-character #\} (get-macro-character #\)))
+
+(set-dispatch-macro-character #\# #\{
+                              #'(lambda (stream char1 char2)
+                                  (let ((accum nil)
+                                        (pair (read-delimited-list #\} stream t)))
+                                    (do ((i (car pair) (+ i 1)))
+                                        ((> i (cadr pair))
+                                         (list 'quote (nreverse accum)))
+                                      (push i accum)))))
