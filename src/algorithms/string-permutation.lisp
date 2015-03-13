961b14e396e5cf3bae1dac87511ebcbd08f8d8a4
HerculesShek <hercules.shek@gmail.com>
2015-03-14 00:10:12 +0800

    string permutation

--- src/algorithms/string-permutation.lisp
+++ src/algorithms/string-permutation.lisp
@@ -2,17 +2,17 @@
 
 ;; string permutation
 (defun string-permutation (str)
-  )
+  (u "" str))
 
 (defun u (c rest)
-  (let lst
+  (let (lst)
     (if (= 1 (length rest))
-        (cons (concatenate 'string c rest) lst)
-        (dolist (e rest)
-          
-
-
-
+        (push (format nil "~A~A" c rest) lst)
+        (do ((i 0 (1+ i)))
+            ((= i (length rest)))
+          (dolist (s (u (char rest i) (make-str rest i)))
+            (push (format nil "~A~A" c s) lst))))
+    lst))
 
 (defun make-str (str i)
   (concatenate 'string (subseq str 0 i) (subseq str (1+ i))))
