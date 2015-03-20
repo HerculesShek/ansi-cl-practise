f67d70134fb77629186dc39fa14eea98127ee142
HerculesShek <hercules.shek@gmail.com>
2015-03-20 22:27:43 +0800

    merge sort

--- src/algorithms/sort/merge-sort.lisp
+++ src/algorithms/sort/merge-sort.lisp
@@ -11,13 +11,39 @@
 
 (defun my-merge (arr from mid to)
   (let ((l-arr (make-array (1+ (- mid from))))
-        (r-arr (make-array (- to mid))))
+        (r-arr (make-array (- to mid)))
+        (l-idx 0)
+        (r-idx 0))
     (do ((i 0 (1+ i)))
         ((= i (length l-arr)))
       (setf (svref l-arr i) (svref arr (+ from i))))
     (do ((i 0 (1+ i)))
         ((= i (length r-arr)))
       (setf (svref r-arr i) (svref arr (+ mid i 1))))
+    
+    (do ((idx from (1+ idx)))
+        ((> idx to))
+      (if (< (svref l-arr l-idx) (svref r-arr r-idx))
+          (progn 
+            (setf (svref arr idx) (svref l-arr l-idx))
+            (incf l-idx)
+            (when (= l-idx (length l-arr))
+              (copy-rest arr (1+ idx) r-arr r-idx)
+              (return-from my-merge)))
+          (progn 
+            (setf (svref arr idx) (svref r-arr r-idx))
+            (incf r-idx)
+            (when (= r-idx (length r-arr))
+              (copy-rest arr (1+ idx) l-arr l-idx)
+              (return-from my-merge)))))))
+
+(defun copy-rest (arr idx arr2 idx2)
+  (do ()
+      ((= idx2 (length arr2)))
+    (setf (svref arr idx) (svref arr2 idx2))
+    (incf idx)
+    (incf idx2)))
+
     
     
   
