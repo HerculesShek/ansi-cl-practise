bd8ec8566ec9f44d22cae4d5af0bc2550ab964df
HerculesShek <hercules.shek@gmail.com>
2015-03-29 21:57:21 +0800

    heapify

--- src/algorithms/sort/heap-sort.lisp
+++ src/algorithms/sort/heap-sort.lisp
@@ -2,6 +2,19 @@
 (defun heap-sort (arr)
   )
 
+(defparameter heap-size 0)
 
 (defun max-heapify (a i)
+  (let ((l (1- (* 2 (1+ i))))
+        (r (* 2 (1+ i)))
+        (largest i))
+    (if (and (< l heap-size) (> (svref a l) (svref a largest)))
+        (setf largest l))
+    (if (and (< r heap-size) (> (svref a r) (svref a largest)))
+        (setf largest r))
+    (when (/= largest i)
+      (rotatef (svref a i) (svref a largest))
+      (max-heapify a largest))))
+      
+        
   
