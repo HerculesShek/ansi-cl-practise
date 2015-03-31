3be6611dce697145f4cc5a63eca6073a01e3eb33
HerculesShek <hercules.shek@gmail.com>
2015-03-31 17:30:45 +0800

    heap sort

--- src/algorithms/sort/heap-sort.lisp
+++ src/algorithms/sort/heap-sort.lisp
@@ -1,6 +1,14 @@
 ;; heap sort by array
+(defparameter heap-size 0)
+
 (defun heap-sort (arr)
-  )
+  (setf heap-size (length arr))
+  (build-max-heap arr)
+  (do ((i (length arr) (1- i)))
+      ((< i 2))
+    (rotatef (svref arr 0) (svref arr (1- i)))
+    (decf heap-size)
+    (max-heapify arr 0)))
 
 (defun build-max-heap (a)
   (let ((from (floor (/ heap-size 2))))
@@ -8,7 +16,6 @@
         ((< i 1))
       (max-heapify a (1- i)))))
 
-(defparameter heap-size 0)
 
 (defun max-heapify (a i)
   (let ((l (1- (* 2 (1+ i))))
