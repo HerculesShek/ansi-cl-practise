ef01a09951a7370ddd367bc4545bebf40bcf6a7e
HerculesShek <hercules.shek@gmail.com>
2015-04-02 22:37:13 +0800

    bucket-sort

--- src/algorithms/sort/bucket-sort.lisp
+++ src/algorithms/sort/bucket-sort.lisp
@@ -1,4 +1,12 @@
 ;;
 
 (defun bucket-sort (arr)
-)
+  (let* ((len (length arr))
+         (b (make-array len)))
+    ))
+
+(defun insert-sort (arr)
+  (if (<= 1 (length arr))
+      (return-from insert-sort)
+      
+
